c984a83a3370d0bd64423add70f9a69d854a610c
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2025-10-03 23:13:47 +0530

    bh

---
+++ server/routes/simulation.routes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import {
+    createSimulation,
+    getSimulations,
+    updateSimulation,
+    deleteSimulation,
+    getSimulationWithBehavior,
+    getOneSimulation
+} from '../controllers/simulation.controller.js';
+import { authenticateToken } from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.post('/', authenticateToken, createSimulation);
+router.get('/:userId', authenticateToken, getSimulations);
+router.get('/:userId/:id', getOneSimulation);
+router.get('/:userId/:simulationId/behavior', authenticateToken, getSimulationWithBehavior);
+router.put('/:id', authenticateToken, updateSimulation);
+router.delete('/:id', authenticateToken, deleteSimulation);
+
+export default router;
